0014165e2cb2f54068f30203d08f2e10abaa98da
Hygor Rasec <hygorrasec@gmail.com>
2024-06-22 22:45:39 -0300

    Chat funcionando 100% e adicionado passo a passo no readme como configurar e executar no windows.

--- README.md
+++ README.md
@@ -11,3 +11,12 @@
  - Makemigrations: ```python manage.py makemigrations```
  - Migrate: ```python manage.py migrate --run-syncdb```
  - Iniciar servidor: ```python manage.py runserver```
+
+### Redis
+
+ - (Windows) Instale o Redis que está na pasta raiz e acessar usando o Power Shell modo administrador: ```cd "C:\Program Files\Redis"```
+ - Visualizar se existe algum processo sendo executado: ```netstat -aon | findstr 6379```
+ - Se tiver e precisar finalizar, use o comando: ```taskkill /PID <PID> /F```
+ - Verificar se finalizou: ```netstat -aon | findstr 6379```
+ - Iniciar o Redis: ```.\redis-server.exe```
+ - Iniciando servidor usando daphne: ```daphne -p 8000 p2_django.asgi:application```
--- autenticacao/consumers.py
+++ autenticacao/consumers.py
@@ -3,42 +3,38 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.group_name = 'chat'
-
-        # Join room group
+        self.room_group_name = 'chat'
         await self.channel_layer.group_add(
-            self.group_name,
+            self.room_group_name,
             self.channel_name
         )
-
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(
-            self.group_name,
+            self.room_group_name,
             self.channel_name
         )
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        username = text_data_json['username']
 
-        # Send message to room group
         await self.channel_layer.group_send(
-            self.group_name,
+            self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'username': username
             }
         )
 
-    # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        username = event['username']
 
-        # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'username': username
         }))
